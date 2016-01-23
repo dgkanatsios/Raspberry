9e62ff5daa42247cd0da5c01f67557966a226ca7
dgkanatsios <dgkanatsios@outlook.com>
2016-01-23 22:29:31 +0200

    Added snake game

---
+++ SenseHatGames/SenseHatGames/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SenseHatGames
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            CoreWindow.GetForCurrentThread().KeyUp += MainPage_KeyUp1;
+        }
+
+
+
+        private Snake.Movement movement = Snake.Movement.Left;
+
+        private void MainPage_KeyUp1(CoreWindow sender, KeyEventArgs args)
+        {
+            if (args.VirtualKey == Windows.System.VirtualKey.Up)
+                movement = Snake.Movement.Top;
+            else if (args.VirtualKey == Windows.System.VirtualKey.Down)
+                movement = Snake.Movement.Bottom;
+            else if (args.VirtualKey == Windows.System.VirtualKey.Left)
+                movement = Snake.Movement.Left;
+            else if (args.VirtualKey == Windows.System.VirtualKey.Right)
+                movement = Snake.Movement.Right;
+        }
+
+        DispatcherTimer timer = new DispatcherTimer();
+        SnakeGame game = new SnakeGame();
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            Draw();
+
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            game.snake.TryMove(movement);
+            Draw();
+        }
+
+        private void Draw()
+        {
+            mainGrid.Children.Clear();
+            for (int row = 0; row < 8; row++)
+            {
+                for (int column = 0; column < 8; column++)
+                {
+                    if (game.matrix[row, column] != null)
+                    {
+                        mainGrid.Children.Add(GetNewEllipse(row, column));
+                    }
+                }
+            }
+        }
+
+        private Ellipse GetNewEllipse(int row, int column)
+        {
+            Ellipse el = new Ellipse();
+            el.Width = el.Height = 40;
+            el.Fill = new SolidColorBrush(Colors.Black);
+            Grid.SetColumn(el, column);
+            Grid.SetRow(el, row);
+            return el;
+        }
+
+    }
+}
---
+++ SenseHatGames/SenseHatGames/SnakeGame.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenseHatGames
+{
+    public class SnakeGame
+    {
+        private Random Randomizer = new Random();
+        public static int Rows = 8;
+        public static int Columns = 8;
+
+        public SnakePiece[,] matrix;
+
+        public Snake snake;
+
+        public SnakeGame()
+        {
+            matrix = new SnakePiece[Rows, Columns];
+            snake = new Snake(matrix);
+            snake.Add(new SnakePiece(2, 2,matrix));
+            snake.Add(new SnakePiece(2, 3,matrix));
+            snake.Add(new SnakePiece(2, 4,matrix));
+        }
+    }
+
+    public class Snake : List<SnakePiece>
+    {
+        private SnakePiece[,] matrix;
+
+        public Snake(SnakePiece[,] array)
+        {
+            if (array == null)
+                throw new ArgumentException("array");
+            matrix = array;
+        }
+
+        public new void Add(SnakePiece piece)
+        {
+            base.Add(piece);
+            matrix[piece.Row, piece.Column] = piece;
+        }
+
+        public bool TryMove(Movement direction)
+        {
+            SnakePiece head = this[0];
+            var pieceLocation = head.RowColumn;
+            switch (direction)
+            {
+                case Movement.Left:
+                    if (head.Column == 0 || matrix[ head.Row, head.Column - 1] != null)
+                        return false;
+                    else
+                        head.Move( head.Row, head.Column - 1);
+                    break;
+                case Movement.Right:
+                    if (head.Column == SnakeGame.Columns - 1 || matrix[head.Row, head.Column + 1] != null)
+                        return false;
+                    else
+                        head.Move( head.Row, head.Column + 1);
+                    break;
+                case Movement.Top:
+                    if (head.Row == 0 || matrix[ head.Row - 1, head.Column] != null)
+                        return false;
+                    else
+                        head.Move( head.Row - 1, head.Column);
+                    break;
+                case Movement.Bottom:
+                    if (head.Row == SnakeGame.Rows - 1 || matrix[ head.Row + 1, head.Column] != null)
+                        return false;
+                    else
+                        head.Move( head.Row + 1, head.Column);
+                    break;
+                default:
+                    return false;
+            }
+
+            for (int i = 1; i < this.Count; i++)
+            {
+                var cache = this[i].RowColumn;
+                this[i].Move(pieceLocation.Item1, pieceLocation.Item2);
+                pieceLocation = cache;
+            }
+            //last one will be null
+            matrix[pieceLocation.Item1, pieceLocation.Item2] = null;
+            return true;
+        }
+
+        public enum Movement { Left, Right, Top, Bottom }
+    }
+
+    public class SnakePiece
+    {
+        public Tuple<int, int> RowColumn
+        {
+            get
+            {
+                return new Tuple<int, int>(Row, Column);
+            }
+        }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        private SnakePiece[,] matrix;
+
+        public SnakePiece(int row, int column, SnakePiece[,] _matrix)
+        { Row = row; Column = column; matrix = _matrix; }
+
+        public void Move(int newRow, int newColumn)
+        {
+            Row = newRow;
+            Column = newColumn;
+            matrix[Row, Column] = this;
+        }
+    }
+
+
+}
