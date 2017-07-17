1ad45016cc2d9f7eca91106bdce1923af6e6e19d
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-07-17 14:08:28 +0300

    fix to n00b error

--- MyHomeStatus/client/helpers.js
+++ MyHomeStatus/client/helpers.js
@@ -5,15 +5,21 @@ if (process.env.DEBUG)
     require('request').debug = true;
 
 function parsedht(value) {
+    let isValid = true;
     value.forEach(function (element) {
-        if (element > 100) return undefined;
+        if (element > 100 || element < -100) {
+            isValid = false;
+            return;
+        };
     }, this);
+    if (isValid) {
+        return {
+            temperature: value[0],
+            humidity: value[1],
+            heatIndex: value[2] //https://en.wikipedia.org/wiki/Heat_index
+        };
+    } else return undefined;
 
-    return {
-        temperature: value[0],
-        humidity: value[1],
-        heatIndex: value[2] //https://en.wikipedia.org/wiki/Heat_index
-    }
 }
 
 function postData(data) {
