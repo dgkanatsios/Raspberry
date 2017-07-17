e965fb630fcbb4f7792119934da5bb251aedcfc1
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-07-17 17:47:57 +0300

    some small server validation logic

--- MyHomeStatus/server/helpers.js
+++ MyHomeStatus/server/helpers.js
@@ -3,6 +3,10 @@ function verifyRequestBody(body) {
         throw new Error('body must have humidity and temperature');
     if (!body.credential || body.credential !== process.env.DEVICE_CREDENTIAL)
         throw new Error('wrong device credential');
+    if(body.humidity < -100 || body.humidity> 100)
+        throw new Error('Invalid humidity');
+    if(body.temperature < -100 || body.temperature > 100)
+        throw new Error('Invalid temperature');
 }
 
 function deleteCredentialProperty(body) {
